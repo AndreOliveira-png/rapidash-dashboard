f715d7158315f6b0c3f899ffd66f8e70f79eac0e
Andre Oliveira <oliveira.luiz85@gmail.com>
2022-09-29 13:34:19 -0300

    Initial Commit:

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/styles.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Deu Certo </p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
