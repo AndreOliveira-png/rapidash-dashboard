1ea811a1bc5098afc1c05cca8f8a86ff01fe6463
André Oliveira <73918849+AndreOliveira-png@users.noreply.github.com>
2022-09-30 10:11:14 -0300

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/styles.css';
 import { Text, Flex, Spacer, Image } from '@chakra-ui/react'
 
 function Header() {
+    useEffect(() => {
+        document.title = 'Rapidash Dashboard';
+    });
     return (
         <Flex
             direction='column'
             bg='#2F576D'
-            
+
         >
             <Flex direction='row' align='center'>
-                <Text  as='b' color='white' ml='10%' className="txtHeader" fontSize='2xl'>Bem Vindo SysAdmin</Text>
+                <Text as='b' color='white' ml='10%' className="txtHeader" fontSize='2xl'>Bem Vindo SysAdmin</Text>
                 <Spacer />
-                <Text  as='b' color='white' className="txtHeader" fontSize='2xl'>Rapidash Sistemas</Text>
+                <Text as='b' color='white' className="txtHeader" fontSize='2xl'>Rapidash Sistemas</Text>
                 <Image
                     boxSize='10%'
                     objectFit='cover'
@@ -22,7 +25,7 @@ function Header() {
             <Flex direction='row'>
                 <Text ml='10%' as='b' color='white' className="txtHeader" fontSize='2xl'>Relação entre encomendas entregues </Text>
                 <Spacer />
-                <Text  mr='10%' as='b' color='white' className="txtHeader" fontSize='2xl'>Relação entre entregas por dia </Text>
+                <Text mr='10%' as='b' color='white' className="txtHeader" fontSize='2xl'>Relação entre entregas por dia </Text>
             </Flex>
         </Flex>
     );
