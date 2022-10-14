64f488c10004a6c29fad56cb7733abab748e1e86
caiovsoares <caio.soares@sou.fae.br>
2022-10-14 19:04:53 -0300

    caio

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,91 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/styles.css';
-import {Text, Flex, Divider, Box } from '@chakra-ui/react'
-import {RenderBarChart, RenderAreaChart } from './RenderLineChart';
+import { Text, Flex, Divider, Box } from '@chakra-ui/react';
+import { RenderBarChart, RenderAreaChart } from './RenderLineChart';
 import CustomTable from './CustomTable';
 import CustomTableMobile from './CustomTableMobile';
 import { cores } from '../styles/colors';
-import { useMedia } from 'react-use'
-
-
-
+import { useMedia } from 'react-use';
 
 function Body() {
-    const isMobile = useMedia('(max-width: 40em)')
-    return (
-        
-        !isMobile ? 
-        <Flex direction='column' >
-            <Flex justify='center' bg={cores.backgroundPadrao}>
-                <Divider bg='white' w="80%" h="2px" orientation='horizontal' />
-            </Flex>
+  const isMobile = useMedia('(max-width: 40em)');
 
-            <Flex
-                direction='row'
-                align='flex-start'
-                bg={cores.backgroundPadrao}
-                //bgGradient='linear(to-b, #1F333F 60%,white 0%)'
-                pt='1%'
-            >
+  return !isMobile ? (
+    <Flex direction='column'>
+      <Flex justify='center' bg={cores.backgroundPadrao}>
+        <Divider bg='white' w='80%' h='2px' orientation='horizontal' />
+      </Flex>
+      <Flex
+        direction='row'
+        align='flex-start'
+        bg={cores.backgroundPadrao}
+        //bgGradient='linear(to-b, #1F333F 60%,white 0%)'
+        pt='1%'
+      >
+        <Box
+          w='50%'
+          h='45vh'
+          bg={cores.backgroundSecundario}
+          boxShadow='2xl'
+          ml='10%'
+          borderRadius='md'
+          borderColor={cores.backgroundPadrao}
+          borderWidth='2px'
+        >
+          <RenderAreaChart />
+        </Box>
+        <Box
+          w='29%'
+          h='45vh'
+          bg={cores.backgroundSecundario}
+          boxShadow='2xl'
+          ml='1%'
+          borderRadius='md'
+          borderColor={cores.backgroundPadrao}
+          borderWidth='2px'
+        >
+          <RenderBarChart />
+        </Box>
+      </Flex>
 
-                <Box w='50%' h='45vh' bg={cores.backgroundSecundario} boxShadow='2xl' ml='10%' borderRadius="md" borderColor={cores.backgroundPadrao} borderWidth='2px'>
-                    <RenderAreaChart />
-                </Box>
-                <Box w='29%' h='45vh' bg={cores.backgroundSecundario} boxShadow='2xl' ml='1%' borderRadius="md" borderColor={cores.backgroundPadrao} borderWidth='2px'>
-                    <RenderBarChart />
-                </Box>
+      <CustomTable />
+    </Flex>
+  ) : (
+    <Flex direction='column'>
+      <Flex direction='column' justify='center' bg={cores.backgroundPadrao}>
+        <Text
+          ml='5%'
+          as='b'
+          color='#DADADA'
+          className='txtHeader'
+          fontSize='lg'
+        >
+          Relação entre entregas por dia{' '}
+        </Text>
+        <Divider ml='5%' bg='white' w='90%' h='1px' orientation='horizontal' />
+      </Flex>
 
+      <Flex
+        align='center'
+        justify='center'
+        bg={cores.backgroundPadrao}
+        //bgGradient='linear(to-b, #1F333F 60%,white 0%)'
+        pt='1%'
+      >
+        <Box
+          w='90%'
+          h='250px'
+          bg={cores.backgroundSecundario}
+          boxShadow='2xl'
+          borderRadius='md'
+          borderColor={cores.backgroundPadrao}
+          borderWidth='2px'
+        >
+          <RenderAreaChart />
+        </Box>
+      </Flex>
 
-
-            </Flex>
-
-            <CustomTable />
-        </Flex>
-        :
-        <Flex direction='column'>
-            <Flex direction='column' justify='center' bg={cores.backgroundPadrao}>
-                <Text ml='5%' as='b' color='#DADADA' className="txtHeader" fontSize='lg'>Relação entre entregas por dia </Text>
-                <Divider ml='5%' bg='white' w="90%" h="1px" orientation='horizontal' />
-            </Flex>
-            
-            <Flex
-                align='center'
-                justify='center'
-                bg={cores.backgroundPadrao}
-                //bgGradient='linear(to-b, #1F333F 60%,white 0%)'
-                pt='1%'
-            >
-                
-                <Box w='90%' h='250px' bg={cores.backgroundSecundario} boxShadow='2xl' borderRadius="md" borderColor={cores.backgroundPadrao} borderWidth='2px'>  
-              
-                    <RenderAreaChart />
-
-                </Box>
-            </Flex>
-            
-            <Flex direction='column' pt='5%' justify='center' bg={cores.backgroundPadrao}>
-                <Text ml='5%' as='b' color='#DADADA' className="txtHeader" fontSize='lg'>Relação entre encomendas entregues </Text>
-                <Divider ml='5%' bg='white' w="90%" h="1px" orientation='horizontal' />
-            </Flex>
-            <Flex
-                align='center'
-                justify='center'
-                bg={cores.backgroundPadrao}
-                //bgGradient='linear(to-b, #1F333F 60%,white 0%)'
-                pt='1%'
-            >
-                
-                <Box w='90%' h='250px' bg={cores.backgroundSecundario} boxShadow='2xl' borderRadius="md" borderColor={cores.backgroundPadrao} borderWidth='2px'>  
-                
-                    <RenderBarChart />
-                
-                </Box>
-            </Flex>
-            <CustomTableMobile />
-        </Flex>
-       
-
-    );
+      <Flex
+        direction='column'
+        pt='5%'
+        justify='center'
+        bg={cores.backgroundPadrao}
+      >
+        <Text
+          ml='5%'
+          as='b'
+          color='#DADADA'
+          className='txtHeader'
+          fontSize='lg'
+        >
+          Relação entre encomendas entregues{' '}
+        </Text>
+        <Divider ml='5%' bg='white' w='90%' h='1px' orientation='horizontal' />
+      </Flex>
+      <Flex
+        align='center'
+        justify='center'
+        bg={cores.backgroundPadrao}
+        //bgGradient='linear(to-b, #1F333F 60%,white 0%)'
+        pt='1%'
+      >
+        <Box
+          w='90%'
+          h='250px'
+          bg={cores.backgroundSecundario}
+          boxShadow='2xl'
+          borderRadius='md'
+          borderColor={cores.backgroundPadrao}
+          borderWidth='2px'
+        >
+          <RenderBarChart />
+        </Box>
+      </Flex>
+      <CustomTableMobile />
+    </Flex>
+  );
 }
 
 export default Body;
